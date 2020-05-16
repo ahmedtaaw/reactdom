dd0020c7bb09d731a30b6399300fc44c1fed45f8
ahmed tawfik <ahmedtaaw@gmail.com>
2020-05-16 17:33:42 +0400

    react updates

---
+++ nestedapps/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Child from './child';
+
+function App() {
+  return (
+    <div className="App">
+        <Child/>
+      </div>
+    )
+  
+}
+
+export default App;
---
+++ nestedapps/src/child.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Child extends Component{
+    render(){
+        return(
+            <div>
+                Child info.
+            </div>
+        )
+    }
+}
+
+export default Child;
