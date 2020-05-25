b5d803bd036908fb37fcf9436115402ccc4804c5
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-26 00:10:09 +0400

    events

--- nestedapps/src/App.js
+++ nestedapps/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Child from './child';
 
-function App() {
-  return (
-    <div className="App">
-        <Child/>
-      </div>
-    )
-  
+class App extends Component {
+    handleClick(){
+        console.log("clicked")
+    }
+    handleMouse(){
+        console.log("mouse")
+    }
+    render() {
+        return ( 
+        <div className = "App" >
+            <Child / >
+
+            <button onClick={this.handleClick}>handle click</button>
+            <button onMouseMove={this.handleMouse}>MouseMove</button>
+        </div>
+        )
+
+    }
 }
 
 export default App;
