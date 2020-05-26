c89c235cf929cfb63b34a664e06b4807bb3b877a
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-26 19:07:37 +0400

    setState

--- nestedapps/src/App.js
+++ nestedapps/src/App.js
@@ -4,7 +4,9 @@ import Child from './child';
 
 class App extends Component {
     state={
-        name:"hamza"
+        name:"hamza",
+        age:35,
+        color:"intial"
     }
     handleClick = () => {
         console.log(this.state.name)
@@ -12,13 +14,26 @@ class App extends Component {
     handleMouse(){
         console.log("mouse")
     }
+
+    changeStage = ()=>{
+        this.setState({
+            name:"ahmed tawfik",
+            age:32,
+            color:"red"
+        })
+    }
+
     render() {
         return ( 
         <div className = "App" >
             <Child / >
 
             <button onClick={this.handleClick}>handle click</button>
-            <button onMouseMove={this.handleMouse}>MouseMove</button>
+            <button onMouseMove={this.handleMouse}>MouseMove</button> 
+            <p></p>
+            <p style={{color:this.state.color}}>state is: name {this.state.name} and age is {this.state.age}</p>
+            <button onClick={this.changeStage}>change state</button>
+
         </div>
         )
 
