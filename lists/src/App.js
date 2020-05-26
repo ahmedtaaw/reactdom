8ebf6826e74b5b48b73aa3bf4cee911e83151de5
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-26 21:44:54 +0400

    pass state from parent

--- lists/src/App.js
+++ lists/src/App.js
@@ -3,10 +3,21 @@ import React, {Component} from 'react';
 import Items from './components/items'
 
 class App extends Component {
+
+  state={
+    items:[
+      {id:1, name:"ahmed", age:"33" },
+      {id:2, name:"amohahmed", age:"22" },
+      {id:3, name:"aya", age:"42" },
+      {id:4, name:"mona", age:"12" }
+    ]
+  }
+
+
   render(){
     return (
       <div className="App">
-        <Items id="1" name="mohamed" age="33"/>
+        <Items items={this.state.items}/>
       </div>
     );
   }
