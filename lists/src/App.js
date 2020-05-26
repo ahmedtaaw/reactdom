434be8710ef1a7ca20a83b3fcfae568738ed8731
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-26 21:34:30 +0400

    import child to parent component

--- lists/src/App.js
+++ lists/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
+import Items from './components/items'
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <Items/>
       </div>
     );
   }
