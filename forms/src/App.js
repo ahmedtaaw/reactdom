51cd20febc222806cba7d9e2db9f0a104c2d82f0
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-30 20:23:02 +0400

    component data

--- forms/src/App.js
+++ forms/src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 //import Formsapp from './components/forms';
 import Outputcondition from './components/outputcondition'
 class App extends Component {
+  state={
+    items:[
+      {id:1, name:"ahmed", age:"33" },
+      {id:2, name:"amohahmed", age:"22" },
+      {id:3, name:"aya", age:"42" },
+      {id:4, name:"mona", age:"12" },
+      {id:5, name:"ahmed", age:"33" },
+      {id:6, name:"amohahmed", age:"22" },
+      {id:7, name:"aya", age:"42" },
+      {id:8, name:"mona", age:"12" }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <Outputcondition/>
+        <Outputcondition items={this.state.items}/>
       </div>
     )
   }
--- forms/src/components/outputcondition.js
+++ forms/src/components/outputcondition.js
@@ -2,10 +2,30 @@ import React, {Component} from 'react';
 
 class Outputcondition extends Component{
     render(){
+        const {items}=this.props;
+        
+        const theItems=items.map((item)=>{
+            return(
+                <tr key={Math.random()*100}>
+                <td>
+                 {item.id}
+                </td>
+                <td>
+                 {item.name}
+                </td>
+                <td>
+                 {item.age}
+                </td>
+            </tr>
+            )
+        })
+
         return(
-            <div>
-                Outputcondition
-            </div>
+            <table>
+                <tbody>
+                    {theItems}
+                    </tbody>
+                    </table>
         )
     }
 }
