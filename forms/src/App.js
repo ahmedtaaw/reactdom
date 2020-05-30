1a952c50e7ca019f0fcf0a440d98f61cc2b144e3
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-30 18:42:49 +0400

    init forms component

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Formsapp from './components/forms';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Formsapp/>
       </div>
     )
   }
---
+++ forms/src/components/forms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Formsapp extends Component{
+    render(){
+        return(
+            <div>
+                Hello Forms
+            </div>
+        )
+    }
+}
+
+export default Formsapp
