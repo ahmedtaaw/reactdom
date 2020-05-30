9eade6e50c6413af7b7bd0b6b49fe35a89d20cb3
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-30 20:13:42 +0400

    forms fields

--- forms/src/components/forms.js
+++ forms/src/components/forms.js
@@ -11,14 +11,29 @@ class Formsapp extends Component{
             name:e.target.value
         })
     }
-
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        this.setState({
+            name:"submitted"
+        })
+    }
+    handleSelect = (e)=>{
+        this.setState({
+            name:e.target.value
+        })
+    }
     render(){
         return(
             <div>
                 <h1>Forms</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" onChange={this.handleChange}></input>
-                    <button>submit</button>
+                    <button value="submit">submit</button>
+                    <select onChange={this.handleSelect} >
+                        <option value="option 1">option 1 </option>
+                        <option value="option 2">option 2</option>
+                        <option value="option 3">option 3 </option>
+                    </select>
                     <h2>{this.state.name}</h2>
                 </form>
             </div>
