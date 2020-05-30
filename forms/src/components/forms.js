9e633107e559533565e9201d52d793096bc4bfb3
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-30 18:52:58 +0400

    update state while typing

--- forms/src/components/forms.js
+++ forms/src/components/forms.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class Formsapp extends Component{
+
+    state = {
+        name:''
+    }
+
+    handleChange = (e)=>{
+        this.setState({
+            name:e.target.value
+        })
+    }
+
     render(){
         return(
             <div>
-                Hello Forms
+                <h1>Forms</h1>
+                <form>
+                    <input type="text" onChange={this.handleChange}></input>
+                    <button>submit</button>
+                    <h2>{this.state.name}</h2>
+                </form>
             </div>
         )
     }
