7c6baba0b27e0f9ec1b1f8250d2aa13e86598053
ahmedtaaw <ahmedtaaw@gmail.com>
2020-05-30 20:18:29 +0400

    output condition init

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Formsapp from './components/forms';
-
+//import Formsapp from './components/forms';
+import Outputcondition from './components/outputcondition'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Formsapp/>
+        <Outputcondition/>
       </div>
     )
   }
---
+++ forms/src/components/outputcondition.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Outputcondition extends Component{
+    render(){
+        return(
+            <div>
+                Outputcondition
+            </div>
+        )
+    }
+}
+
+export default Outputcondition;
