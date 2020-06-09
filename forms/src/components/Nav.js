134df7a63c0717aa418160ef7aae34be2eef6735
ahmedtaaw <ahmedtaaw@gmail.com>
2020-06-10 02:12:39 +0400

    Navlink

--- forms/src/components/Nav.js
+++ forms/src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-
+import {Link, NavLink} from 'react-router-dom';
 
 const Nav = () =>{
     return(
         <div>
             <a href='#'>Logo</a>
             <ul>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
+                <NavLink activeClassName='selected' exact to="/">Home</NavLink>
+                <NavLink activeClassName='selected' to="/about">About</NavLink>
             </ul>
         </div>
     )
